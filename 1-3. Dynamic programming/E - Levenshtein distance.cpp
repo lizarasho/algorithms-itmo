ef26ebd0ac03ea7fbc45628a03954d8e549e4c41
lizarasho <47846151+lizarasho@users.noreply.github.com>
2019-11-16 16:28:32 +0300

    Some refactoring

--- 1-3. Dynamic programming/E - Levenshtein distance.cpp
+++ 1-3. Dynamic programming/E - Levenshtein distance.cpp
@@ -4,17 +4,22 @@
 #include <vector>
 #include <limits.h>
 
+#define INF LONG_LONG_MAX
+
 using namespace std;
-const int N = 1000;
-long long dp[N + 1][N + 1];
+
+vector<vector<long long>> dp;
 
 int main()
 {
-    int n;
     string a, b;
     cin >> a;
     cin >> b;
+
+    dp.resize(a.size() + 1, vector<long long>(b.size() + 1, INF));
+
     for (int i = 0; i <= a.size(); i++)
+    {
         for (int j = 0; j <= b.size(); j++)
         {
             if (i == 0 || j == 0)
@@ -22,10 +27,11 @@ int main()
                 dp[i][j] = i + j;
                 continue;
             }
-            dp[i][j] = LONG_LONG_MAX;
             if (i > 0 && j > 0 && a[i - 1] == b[j - 1])
+            {
                 dp[i][j] = dp[i - 1][j - 1];
-            else
+                continue;
+            }
             if (i > 0 && j > 0)
             {
                 dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j - 1]);
@@ -33,5 +39,7 @@ int main()
                 dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j]);
             }
         }
+    }
+
     cout << dp[a.length()][b.length()];
 }
