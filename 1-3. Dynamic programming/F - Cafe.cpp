66cb770c125c3756e599986fdee7446c1291f902
lizarasho <47846151+lizarasho@users.noreply.github.com>
2019-11-16 16:22:04 +0300

    Some refactoring

--- 1-3. Dynamic programming/F - Cafe.cpp
+++ 1-3. Dynamic programming/F - Cafe.cpp
@@ -3,81 +3,86 @@
 #include <string>
 #include <vector>
 #include <limits.h>
+
 using namespace std;
 
+#define INF INT_MAX
+
 const int N = 100;
-long long dp[N + 1][N + 1];
-int c[N + 1];
+
+vector<vector<long long>> dp;
+vector<int> c(N + 1);
 
 int main()
 {
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
+
+    dp.resize(N + 1, vector<long long>(N + 1, 0));
+    for (int i = 1; i < n + 1; i++)
         cin >> c[i];
 
-    for (int i = 0; i <= n; i++)
-        for (int j = 0; j <= n; j++)
-            dp[i][j] = 0;
-
-    for (int i = 0; i <= n; i++)
-        for (int j = i + 1; j <= n; j++)
-            dp[i][j] = INT_MAX;
+    for (int i = 0; i < n + 1; i++)
+        for (int j = i + 1; j < n + 1; j++)
+            dp[i][j] = INF;
 
     dp[1][0] = c[1];
+    dp[1][1] = (c[1] > N) ? c[1] : INF;
 
-    if (c[1] > N)
-        dp[1][1] = c[1];
-    else
-        dp[1][1] = INT_MAX;
+    for (int i = 2; i < n + 1; i++)
+        dp[i][0] = INF;
 
-    for (int i = 2; i <= n; i++)
-        dp[i][0] = INT_MAX;
-
-    for (int i = 2; i <= n; i++)
-        for (int j = 0; j <= n; j++)
+    for (int i = 2; i < n + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
         {
             if (c[i] <= N)
-                dp[i][j] = min(dp[i - 1][j] + c[i], dp[i - 1][j + 1]);
-            else
             {
-                if (j > 0)
-                    dp[i][j] = min(dp[i - 1][j - 1] + c[i], dp[i - 1][j + 1]);
-                else
-                    dp[i][j] = dp[i - 1][j + 1];
+                dp[i][j] = min(dp[i - 1][j] + c[i], dp[i - 1][j + 1]);
+            } else if (j > 0)
+            {
+                dp[i][j] = min(dp[i - 1][j - 1] + c[i], dp[i - 1][j + 1]);
+            } else
+            {
+                dp[i][j] = dp[i - 1][j + 1];
             }
 
-            if (dp[i][j] > INT_MAX)
-                dp[i][j] = INT_MAX;
-        }
-
-
-    int k1 = 0;
-    while (k1 < n && dp[n][k1] >= dp[n][k1 + 1] && dp[n][k1 + 1] > 0)
-        k1++;
-
-    cout << dp[n][k1] << endl;
-    cout << k1 << " ";
-    int str = n;
-    int stl = k1;
-    vector<int> days;
-    while (str > 0)
-    {
-        if (dp[str][stl] == dp[str - 1][stl + 1])
-        {
-            days.push_back(str);
-            str--;
-            stl++;
-        }
-        else
-        {
-            str--;
-            if (c[str + 1] > 100)
-                stl--;
+            if (dp[i][j] > INF)
+            {
+                dp[i][j] = INF;
+            }
         }
     }
-    cout << days.size() << endl;
+
+
+    int col = 0;
+    while (col < n && dp[n][col] >= dp[n][col + 1] && dp[n][col + 1] > 0)
+        col++;
+
+    cout << dp[n][col] << '\n';
+    cout << col << ' ';
+
+    int line = n;
+
+    vector<int> days;
+    while (line > 0)
+    {
+        if (dp[line][col] == dp[line - 1][col + 1])
+        {
+            days.push_back(line);
+            line--;
+            col++;
+            continue;
+        }
+        line--;
+        if (c[line + 1] > 100)
+            col--;
+    }
+
+    cout << days.size() << '\n';
 
     for (int i = days.size() - 1; i >= 0; i--)
-        cout << days[i] << endl;
+        cout << days[i] << '\n';
+
+    return 0;
 }
